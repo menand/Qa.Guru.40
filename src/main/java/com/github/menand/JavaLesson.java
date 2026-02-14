bcf01d5f6bfb9d69559e4bf760b0551c3038c8f5
Andrey Menshov <menand@yandex.ru>
2026-02-14 12:24:50 +0300

    Lesson Java, part1. HomeWork.

---
+++ src/main/java/com/github/menand/JavaLesson.java
@@ -0,0 +1,26 @@
+package com.github.menand;
+
+public class JavaLesson {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = -10;
+        double d1 = 0.1;
+        double dmax = Double.MAX_VALUE;
+        int imax = Integer.MAX_VALUE;
+        int imin = Integer.MIN_VALUE;
+
+        System.out.println(i1 + i2);
+        System.out.println(i1 - i2);
+        System.out.println(i1 * i2);
+        System.out.println(i1 / i2);
+        System.out.println(i1 / d1);
+        System.out.println(i1 * d1);
+        System.out.println(i1 < d1);
+        System.out.println(i1 > i2);
+        System.out.println(i1 >= dmax);
+        System.out.println(i1 <= dmax);
+        System.out.println(--imin);
+        System.out.println(++imax);
+        System.out.println(dmax*dmax);
+    }
+}
