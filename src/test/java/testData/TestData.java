6f212a4af66c0eb31d5bae57c762bacf7fc39a50
Andrey Menshov <menand@yandex.ru>
2026-02-14 13:05:59 +0300

    Lesson 4, variables in TestData class.

---
+++ src/test/java/testData/TestData.java
@@ -0,0 +1,18 @@
+package testData;
+
+public class TestData {
+    public static String firstName = "Ivan";
+    public static String lastName = "Ivanov";
+    public static String userEmail = "ivanov@mail.ru";
+    public static String gender = "Male";
+    public static String userNumber = "1122334455";
+    public static String yearOfBirth = "1985";
+    public static String monthOfBirth = "June";
+    public static String dayOfBirth = "10";
+    public static String subjects = "Maths";
+    public static String hobbies = "Reading";
+    public static String picturePath = "files/avatar.png";
+    public static String currentAddress = "Sezam street, 1";
+    public static String state = "Haryana";
+    public static String city = "Panipat";
+}
