8bfc71784a487ed70935948d2e9aa8dc1b9d5830
Andrey Menshov <menand@yandex.ru>
2026-03-18 13:43:58 +0300

    banner removeer

--- src/test/java/com/demoqa/pages/DemoqaComParentPage.java
+++ src/test/java/com/demoqa/pages/DemoqaComParentPage.java
@@ -1,19 +1,18 @@
 package com.demoqa.pages;
 
-import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class DemoqaComParentPage {
     protected void removeBanners() {
         executeJavaScript(
                 """
-                document.querySelectorAll('iframe, .adsbygoogle, #fixedban')
-                        .forEach(el => el.remove());
-                """
-        );
-
-        // ждём пока iframe реально исчезнут
-        $$("iframe").shouldHave(size(0));
+                    const style = document.createElement('style');
+                    style.innerHTML = `
+                        iframe, .adsbygoogle, #fixedban, footer {
+                            display: none !important;
+                        }
+                    `;
+                    document.head.appendChild(style);
+                """);
     }
 }
